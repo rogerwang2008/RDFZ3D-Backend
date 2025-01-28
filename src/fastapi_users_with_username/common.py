ae6c8809ea9e5771a3860bee2cd7aa1be8e4602f
rogerwang2008 <rogerwang2008@163.com>
2025-01-28 18:22:25 +0800

    重写注册，用户已经存在时将提供冲突的是什么

---
+++ src/fastapi_users_with_username/common.py
@@ -0,0 +1,7 @@
+import enum
+
+
+class Identifiers(enum.StrEnum):
+    EMAIL = "email"
+    PHONE_NO = "phone_no"
+    USERNAME = "username"
--- src/fastapi_users_with_username/router/__init__.py
+++ src/fastapi_users_with_username/router/__init__.py
@@ -1,2 +1,3 @@
+from .register import get_register_router
 from .reset import get_reset_password_router
 from .verify import get_verify_router
