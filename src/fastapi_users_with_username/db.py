7ef9084313c6dd147d8a38b1e52428b5bba7c345
rogerwang2008 <rogerwang2008@163.com>
2025-01-30 17:51:20 +0800

    代码可读性优化

--- src/fastapi_users_with_username/db.py
+++ src/fastapi_users_with_username/db.py
@@ -1,16 +1,11 @@
 from typing import TYPE_CHECKING, Optional
-import uuid
 
-import datetime
-
-import fastapi_users_db_sqlmodel.generics
 import fastapi_users_db_sqlmodel.access_token
 import sqlalchemy.sql.schema
 import ulid
 
-from pydantic import UUID4, EmailStr
+from pydantic import EmailStr
 from pydantic_extra_types.phone_numbers import PhoneNumber
-from pydantic_extra_types.ulid import ULID
 import sqlmodel
 from sqlalchemy import func
 import fastapi_users_db_sqlmodel
@@ -21,24 +16,18 @@ PhoneNumber.phone_format = "E164"
 
 class SQLModelBaseUserDB(sqlmodel.SQLModel):
     __tablename__ = "user"
-    # id: UUID4 = sqlmodel.Field(default_factory=uuid.uuid4, primary_key=True, nullable=False)
+    # noinspection PyTypeChecker
     id: str = sqlmodel.Field(default_factory=ulid.ULID, primary_key=True, nullable=False, min_length=26, max_length=26,
                              sa_type=sqlalchemy.CHAR(26))
-    username: str = sqlmodel.Field(
-        sa_column_kwargs={"unique": True, "index": True}, nullable=False
-    )
+    username: str = sqlmodel.Field(regex=r"^[a-zA-Z0-9_-]+$", unique=True, index=True, nullable=False)
     if TYPE_CHECKING:
         email: Optional[str]
     else:
-        email: Optional[EmailStr] = sqlmodel.Field(
-            sa_column_kwargs={"unique": True, "index": True}, nullable=True
-        )
+        email: Optional[EmailStr] = sqlmodel.Field(unique=True, index=True, nullable=True)
     if TYPE_CHECKING:
         phone_no: Optional[str]
     else:
-        phone_no: Optional[PhoneNumber] = sqlmodel.Field(
-            sa_column_kwargs={"unique": True, "index": True}, nullable=True
-        )
+        phone_no: Optional[PhoneNumber] = sqlmodel.Field(unique=True, index=True, nullable=True)
     hashed_password: str
     is_active: bool = sqlmodel.Field(True, nullable=False)
     is_superuser: bool = sqlmodel.Field(False, nullable=False)
@@ -51,6 +40,7 @@ class SQLModelBaseUserDB(sqlmodel.SQLModel):
 
 
 class SQLModelBaseAccessToken(fastapi_users_db_sqlmodel.access_token.SQLModelBaseAccessToken):
+    # noinspection PyTypeChecker
     user_id: str = sqlmodel.Field(foreign_key="user.id", nullable=False, sa_type=sqlalchemy.CHAR(26))
 
 
